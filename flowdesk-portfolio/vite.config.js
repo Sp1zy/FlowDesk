ae43838b536c0fa4296b2b035dc98002a648e987
Christian Stråhle <thebonemeal23@gmail.com>
2026-07-01 20:16:24 +0200

    chore: setup automated subfolder github deployment workflow

--- flowdesk-portfolio/vite.config.js
+++ flowdesk-portfolio/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// Replace 'YOUR_REPOSITORY_NAME' with the exact name of your GitHub repo!
 export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss()
-    ],
+  base: '/FlowDesk/',
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 })
